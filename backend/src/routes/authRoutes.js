d130af025941f52c2102dc94986fd2385205765b
Danijel <developedbydane@gmail.com>
2024-11-28 15:31:48 +0100

    feat(auth): Added getUser controller and routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup, login } from "../controllers/authController.js";
+import { signup, login, getUser } from "../controllers/authController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", authMiddleware, getUser);
 
 export default router;
