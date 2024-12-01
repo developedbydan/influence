de2e4c0657f0599a63023f0e9197ece7d96487d0
Danijel <developedbydane@gmail.com>
2024-12-01 21:17:26 +0100

    feat(loading): Added loading spinner

--- frontend/src/pages/AccountSettings.jsx
+++ frontend/src/pages/AccountSettings.jsx
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 import { ToastContainer, toast } from "react-toastify";
 import { fetchUserData, updateUserData } from "../api/auth";
 import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 const AccountSettings = () => {
   const navigate = useNavigate();
@@ -10,6 +11,8 @@ const AccountSettings = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUserDataData = async () => {
@@ -19,8 +22,10 @@ const AccountSettings = () => {
 
         setEmail(email);
         setUsername(username);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching user data:", error);
+        setLoading(false);
       }
     };
 
@@ -62,72 +67,78 @@ const AccountSettings = () => {
   return (
     <Layout>
       <div className="bg-background h-screen text-white pt-32 pb-8 px-4 lg:px-20 font-inter  overflow-y-scroll no-scrollbar ">
-        <div className="w-full md:w-8/12 lg:w-7/12 xl:w-5/12 2xl:w-4/12  rounded-xl bg-gray-800">
-          <div className="px-5 py-8 xl:px-10 ">
-            <h1 className="text-xl pb-8 text-center font-bold md:text-2xl ">
-              Update Info
-            </h1>
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="username"
-                  className="block mb-2 text-sm font-medium "
-                >
-                  Username
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  value={username}
-                  className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
-                  placeholder="Name"
-                  onChange={handleUsernameChange}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium "
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={email}
-                  className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
-                  placeholder="name@company.com"
-                  onChange={handleEmailChange}
-                />
-              </div>
-              <div className="pb-5">
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium "
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
-                  onChange={handlePasswordChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full text-black bg-white hover:bg-gray-200  focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-              >
-                Update
-              </button>
-            </form>
-            <ToastContainer />
+        {loading ? (
+          <div className="flex justify-center items-center h-screen animate-pulse">
+            <ClipLoader color="#6A71F2" size={40} />
           </div>
-        </div>
+        ) : (
+          <div className="w-full md:w-8/12 lg:w-7/12 xl:w-5/12 2xl:w-4/12  rounded-xl bg-gray-800">
+            <div className="px-5 py-8 xl:px-10 ">
+              <h1 className="text-xl pb-8 text-center font-bold md:text-2xl ">
+                Update Info
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium "
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={username}
+                    className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
+                    placeholder="Name"
+                    onChange={handleUsernameChange}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium "
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
+                    placeholder="name@company.com"
+                    onChange={handleEmailChange}
+                  />
+                </div>
+                <div className="pb-5">
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium "
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className=" text-white rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 "
+                    onChange={handlePasswordChange}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-black bg-white hover:bg-gray-200  focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                >
+                  Update
+                </button>
+              </form>
+              <ToastContainer />
+            </div>
+          </div>
+        )}
       </div>
     </Layout>
   );
--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -5,11 +5,13 @@ import { PaperPlaneRight } from "@phosphor-icons/react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { bookInfluencer, getOneInfluencer } from "../api/influencers";
+import { ClipLoader } from "react-spinners";
 
 const Booking = () => {
   const { influencerId } = useParams();
   const [influencer, setInfluncer] = useState([]);
   const [details, setDetails] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
   const bookingFee = 10;
@@ -19,6 +21,7 @@ const Booking = () => {
       try {
         const response = await getOneInfluencer(influencerId);
         setInfluncer(response);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching influencer details:", error);
       }
@@ -46,7 +49,7 @@ const Booking = () => {
         setDetails("");
         handleSuccess(message);
         setTimeout(() => {
-          navigate("/");
+          navigate("/bookings");
         }, 1500);
       } else {
         handleError(message);
@@ -59,59 +62,70 @@ const Booking = () => {
   return (
     <Layout>
       <div className="bg-background h-screen text-white pt-6 pb-8 px-4 lg:px-20 font-inter  overflow-y-scroll no-scrollbar ">
-        <div className="bg-gray-800 flex flex-col items-center pt-10 pb-10 px-4 rounded-2xl">
-          <h2 className="font-bold text-xl">Booking Details</h2>
-          <div className="pt-10 pb-16 flex flex-col items-center \ w-full">
-            <img
-              src={influencer.imageUrl}
-              alt="Influencer image"
-              className="w-28 h-28 rounded-full object-cover object-center"
-            />
-
-            <h3 className="pt-4 pb-2 font-bold text-lg">{influencer.name}</h3>
-            <div className="bg-buttonSecondary py-1 px-3 rounded-full ">
-              <p className=" text-buttonBlend text-xs font-semibold">
-                {influencer.category}
-              </p>
-            </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-screen animate-pulse">
+            <ClipLoader color="#6A71F2" size={40} />
           </div>
-          <div className=" w-full border-b-2">
-            <div className="w-full flex justify-between text-sm text-gray-400">
-              <p>Ad price:</p>
-              <p>${influencer.price}</p>
-            </div>
-            <div className="w-full flex justify-between text-sm text-gray-400">
-              <p>Booking fee:</p>
-              <p>${bookingFee}</p>
-            </div>
-            <div className="w-full flex justify-between pt-3 font-semibold">
-              <p>Total:</p>
-              <p>${influencer.price + bookingFee}</p>
-            </div>
-          </div>
+        ) : (
+          <>
+            <div className="bg-gray-800 flex flex-col items-center pt-10 pb-10 px-4 rounded-2xl">
+              <h2 className="font-bold text-xl">Booking Details</h2>
+              <div className="pt-10 pb-16 flex flex-col items-center \ w-full">
+                <img
+                  src={influencer.imageUrl}
+                  alt="Influencer image"
+                  className="w-28 h-28 rounded-full object-cover object-center"
+                />
 
-          <form
-            onSubmit={handleSubmit}
-            className="w-full pt-10 flex flex-col items-center gap-10"
-          >
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Details..."
-              className="w-full min-h-36 max-h-36 bg-gray-700 rounded-xl outline-none p-3 text-sm"
-              value={details}
-              onChange={(e) => setDetails(e.target.value)}
-            ></textarea>
-            <button
-              type="submit"
-              className="w-full flex gap-2 items-center justify-center text-black bg-white hover:bg-gray-200  focus:outline-none font-semibold rounded-lg text-sm px-5 py-3 text-cente "
-            >
-              Book a meeting
-              <PaperPlaneRight size={16} weight="fill" />
-            </button>
-          </form>
-        </div>
-        <ToastContainer />
+                <h3 className="pt-4 pb-2 font-bold text-lg">
+                  {influencer.name}
+                </h3>
+                <div className="bg-buttonSecondary py-1 px-3 rounded-full ">
+                  <p className=" text-buttonBlend text-xs font-semibold">
+                    {influencer.category}
+                  </p>
+                </div>
+              </div>
+              <div className=" w-full border-b-2">
+                <div className="w-full flex justify-between text-sm text-gray-400">
+                  <p>Ad price:</p>
+                  <p>${influencer.price}</p>
+                </div>
+                <div className="w-full flex justify-between text-sm text-gray-400">
+                  <p>Booking fee:</p>
+                  <p>${bookingFee}</p>
+                </div>
+                <div className="w-full flex justify-between pt-3 font-semibold">
+                  <p>Total:</p>
+                  <p>${influencer.price + bookingFee}</p>
+                </div>
+              </div>
+
+              <form
+                onSubmit={handleSubmit}
+                className="w-full pt-10 flex flex-col items-center gap-10"
+              >
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Details..."
+                  className="w-full min-h-36 max-h-36 bg-gray-700 rounded-xl outline-none p-3 text-sm"
+                  required
+                  value={details}
+                  onChange={(e) => setDetails(e.target.value)}
+                ></textarea>
+                <button
+                  type="submit"
+                  className="w-full flex gap-2 items-center justify-center text-black bg-white hover:bg-gray-200  focus:outline-none font-semibold rounded-lg text-sm px-5 py-3 text-cente "
+                >
+                  Book a meeting
+                  <PaperPlaneRight size={16} weight="fill" />
+                </button>
+              </form>
+            </div>
+            <ToastContainer />
+          </>
+        )}
       </div>
     </Layout>
   );
