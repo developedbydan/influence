aa76c909dd7aa2e55225285792ab9933fc02202e
Danijel <developedbydane@gmail.com>
2024-12-02 01:20:47 +0100

    (auth): quick fix

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,20 +1,28 @@
+import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = getCookie("token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 const getCookie = (name) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
-};
-
-const ProtectedRoute = ({ children }) => {
-  const token = getCookie("token");
-  console.log("Token from document.cookie:", token);
-
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
+  return null;
 };
 
 export default ProtectedRoute;
